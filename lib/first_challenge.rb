faf1c809f5f26b89dd5688f4b3e6a9dadb8d0739
Asaf Davidov <adavidov@conncoll.edu>
2018-09-11 16:17:45 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,8 @@ def first_challenge
 
   #your code here
   contacts["Freddy Mercury"].each do |attribute, value|
-    
+    if attribute == :favorite_icecream_flavors
+      attribute.delete_if{
 
   #remember to return your newly altered contacts hash!
   contacts
